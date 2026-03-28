def1a94fcc4d20cf2d822583eee687f0659c2b7f
GV-ing <g.vestri2904@gmail.com>
2026-03-28 19:31:42 +0100

    Caricamento iniziale workspace su branch Real

---
+++ docker_scripts/docker_build_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# Start the Docker image build
+rm -rf ./tmp_sources
+mkdir -p ./tmp_sources
+
+# Ensure source files exist before copying
+if [ -d "../src" ]; then
+  cp -r ../src/* ./tmp_sources
+else
+  echo "Error: Source directory ../src does not exist. Exiting."
+  exit 1
+fi
+
+if docker build -t scooby_real_image -f ./Dockerfile .; then
+  rm -rf ./tmp_sources
+  echo "Build completed. Start the container with: ./docker_run_container.sh"
+else
+  rm -rf ./tmp_sources
+  echo "Rerun ./docker_build_image.sh and try again."
+fi
---
+++ src/scooby_description/README.md
@@ -0,0 +1,16 @@
+# scooby Description Package
+
+This package contains urdf descriprion for the Fratomo robot used for the Robotics Lab course
+
+### Launch files
+
+- **Simulazione Gazebo:**
+  ```bash
+  ros2 launch scooby_description scooby_gazebo.launch.py
+  ```
+
+- **Visualizzazione RViz:**
+  ```bash
+  ros2 launch scooby_description scooby_rviz.launch.py
+  ```
+
